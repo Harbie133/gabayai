5a81c2fb7d02d489541f5f1dfbd8c178bbf5b014
Harbie133 <164719853+Harbie133@users.noreply.github.com>
2026-01-02 23:19:12 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";  // your db username
-$password = "";      // your db password
-$dbname = "gabayai";
+$servername = "sql310.infinityfree.com";
+$username = "if0_40257822";  // your db username
+$password = "UcdQSOY1E2HprQQ";      // your db password
+$dbname = "if0_40257822_gabayai";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
